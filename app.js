f5fd9ebf809ee515469feec13080ddf11bebfebc
samualdi <samualdick89@gmail.com>
2021-09-21 15:58:49 +0100

    created error handlers for custom errors, 400 errors and 500 errors and modified app to utilise them through error handling middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require('./db/connection');
 const apiRouter = require('./routes/api.router');
+const { handle500Errors, handle400Errors, handleCustomErrors } = require('./errors/error.handlers');
 
 
 const app = express();
@@ -9,9 +10,9 @@ app.use(express.json());
 
 app.use('/api', apiRouter);
 
-app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(500).send({ msg: 'Internal Server Error' })
-})
+app.use(handle400Errors);
+app.use(handleCustomErrors);
+
+app.use(handle500Errors);
 
 module.exports = app;
---
+++ errors/error.handlers.js
@@ -0,0 +1,23 @@
+
+
+exports.handle500Errors = (err, req, res, next) => {
+    console.log(err);
+    res.status(500).send({ msg: 'Internal Server Error' })
+}
+
+exports.handle400Errors = (err, req, res, next) => {
+    if (err.code === '22P02') {
+        res.status(400).send({ msg: "Bad request" });
+    } else {
+        next(err);
+    }
+}
+
+exports.handleCustomErrors = (err, req, res, next) => {
+    if (err.status) {
+        res.status(err.status).send({ msg: err.msg });
+    } else {
+        next(err);
+    }
+    
+}
